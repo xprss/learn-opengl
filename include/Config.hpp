0e1eed5e804e385d7d8054f6d9f37a4ecc8164f5
xprss <vincenzo.sagristano@yahoo.com>
2025-12-17 22:41:07 +0100

    added config namespace

---
+++ include/Config.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Config
+{
+    // Window sizes
+    constexpr int WINDOW_WIDTH = 600;
+    constexpr int WINDOW_HEIGHT = 800;
+
+    // Window title
+    constexpr char WINDOW_TITLE[] = "xprss learning OpenGL";
+
+    // Initial colors
+    constexpr float CLEAR_R = 0.0f;
+    constexpr float CLEAR_G = 0.0f;
+    constexpr float CLEAR_B = 0.0f;
+}
