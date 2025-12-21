3a12be0d6f4672c99c871a8126347abe9fb8cbfc
xprss <80033167+xprss@users.noreply.github.com>
2025-12-21 13:19:07 +0100

    fixed missing buffer clear + added component to visualize currently generated color over the background

--- src/main.cpp
+++ src/main.cpp
@@ -38,35 +38,36 @@ int main(int argc, char const *argv[])
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGuiIO &io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
     // io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
 
     // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(app.window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplGlfw_InitForOpenGL(app.window, true); // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
     ImGui_ImplOpenGL3_Init();
 
     while (!glfwWindowShouldClose(app.window))
     {
         process_input();
-        // glClearColor(app.red, app.green, app.blue, 1.0f);
-        // glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(app.window);
         glfwPollEvents();
-        // (Your code calls glfwPollEvents())
-        // ...
-        // Start the Dear ImGui frame
+
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-        ImGui::ShowDemoWindow(); // Show demo window! :)
-        // Rendering
-        // (Your code clears your framebuffer, renders your other stuff etc.)
+
+        ImGui::Begin("Color Adjuster");
+        ImGui::Text("Press R/G/B to increase Red/Green/Blue components");
+        ImGui::ColorEdit4("Background Color", *app.colors4);
+        glClearColor(app.red, app.green, app.blue, app.alpha);
+        ImGui::End();
+
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        // (Your code calls glfwSwapBuffers() etc.)
+
+        glfwSwapBuffers(app.window);
+        glClear(GL_COLOR_BUFFER_BIT);
     }
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
@@ -78,13 +79,42 @@ void process_input()
 {
     AppContext &app = AppContext::get();
     if (glfwGetKey(app.window, GLFW_KEY_ESCAPE) == GLFW_PRESS || glfwGetKey(app.window, GLFW_KEY_Q) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(app.window, true);
+    }
     if (glfwGetKey(app.window, GLFW_KEY_R) == GLFW_PRESS)
-        app.red = std::fmod(app.red + 0.001f, 1.0f);
+    {
+        if (!glfwGetKey(app.window, GLFW_KEY_LEFT_SHIFT) && !glfwGetKey(app.window, GLFW_KEY_RIGHT_SHIFT))
+        {
+            app.red = std::fmod(app.red + 0.001f, 1.0f);
+        }
+        else
+        {
+            app.red = std::fmod(app.red - 0.001f, 1.0f);
+        }
+    }
     if (glfwGetKey(app.window, GLFW_KEY_G) == GLFW_PRESS)
-        app.green = std::fmod(app.green + 0.001f, 1.0f);
+    {
+        if (!glfwGetKey(app.window, GLFW_KEY_LEFT_SHIFT) && !glfwGetKey(app.window, GLFW_KEY_RIGHT_SHIFT))
+        {
+            app.green = std::fmod(app.green + 0.001f, 1.0f);
+        }
+        else
+        {
+            app.green = std::fmod(app.green - 0.001f, 1.0f);
+        }
+    }
     if (glfwGetKey(app.window, GLFW_KEY_B) == GLFW_PRESS)
-        app.blue = std::fmod(app.blue + 0.001f, 1.0f);
+    {
+        if (!glfwGetKey(app.window, GLFW_KEY_LEFT_SHIFT) && !glfwGetKey(app.window, GLFW_KEY_RIGHT_SHIFT))
+        {
+            app.blue = std::fmod(app.blue + 0.001f, 1.0f);
+        }
+        else
+        {
+            app.blue = std::fmod(app.blue - 0.001f, 1.0f);
+        }
+    }
 }
 
 void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
