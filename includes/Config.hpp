6eeee98bd444be3f1cb404ba5c423ccb69e1cdd4
xprss <80033167+xprss@users.noreply.github.com>
2025-12-31 11:32:18 +0100

    added some configurations for json savefile

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -8,7 +8,7 @@ namespace Config
     constexpr int WINDOW_HEIGHT = 600;
 
     // Window title
-    constexpr char WINDOW_TITLE[] = "xprss learning OpenGL";
+    constexpr char WINDOW_TITLE[] = "A fantastic palette of colors";
 
     // Initial colors
     constexpr float CLEAR_R = 0.0f;
@@ -21,6 +21,13 @@ namespace Config
     constexpr int COLOR_G_INDEX = 1;
     constexpr int COLOR_B_INDEX = 2;
     constexpr int COLOR_ALPHA_INDEX = 3;
+
+    // JSON savefile keys
+    constexpr const char* JSON_SAVEFILE_COLOR_OBJ_KEY = "color";
+    constexpr const char* JSON_SAVEFILE_COLOR_R_KEY = "R";
+    constexpr const char* JSON_SAVEFILE_COLOR_G_KEY = "G";
+    constexpr const char* JSON_SAVEFILE_COLOR_B_KEY = "B";
+    constexpr const char* JSON_SAVEFILE_COLOR_ALPHA_KEY = "A";
 
     // background color savefile name
     const std::string COLOR_SAVEFILE = "background_color.txt";
