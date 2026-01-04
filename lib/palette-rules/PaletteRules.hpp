90a25ad458c4bcea1c5cf527584cf0c6c44c418c
xprss <80033167+xprss@users.noreply.github.com>
2026-01-04 10:17:08 +0100

    enum to support multiple rules

--- lib/palette-rules/PaletteRules.hpp
+++ lib/palette-rules/PaletteRules.hpp
@@ -1,3 +1,42 @@
 #pragma once
 #include "ColorPaletteRule.hpp"
 #include "ArmonicTriad.hpp"
+#include "Inverse.hpp"
+#include <array>
+
+namespace Palette
+{
+    enum class PaletteRuleType
+    {
+        ARMONIC_TRIAD,
+        INVERSE
+    };
+
+    constexpr std::array<PaletteRuleType, 2> paletteRuleTypes = {{PaletteRuleType::ARMONIC_TRIAD,
+                                                                  PaletteRuleType::INVERSE}};
+
+    inline ColorPaletteRule *createPaletteRule(PaletteRuleType type)
+    {
+        switch (type)
+        {
+        case PaletteRuleType::ARMONIC_TRIAD:
+            return new ArmonicTriad();
+        case PaletteRuleType::INVERSE:
+            return new Inverse();
+        default:
+            throw std::invalid_argument("Unknown PaletteRuleType");
+        }
+    }
+
+    constexpr std::string_view to_string(PaletteRuleType t)
+    {
+        switch (t)
+        {
+        case PaletteRuleType::ARMONIC_TRIAD:
+            return "Armonic Triad";
+        case PaletteRuleType::INVERSE:
+            return "Inverse";
+        }
+        return "UNKNOWN";
+    }
+}
